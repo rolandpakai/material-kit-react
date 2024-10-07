b69310b4d2382b1da0691231738ed72fd6445f08
roland.pakai <roland.pakai@lightware.com>
2024-10-07 10:09:06 +0200

    Extend TimeSpent component with title, label, percentage props

--- src/components/dashboard/overview/time-spent.tsx
+++ src/components/dashboard/overview/time-spent.tsx
@@ -6,13 +6,17 @@ import Stack from '@mui/material/Stack';
 import type { SxProps } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import { Timer as TimerIcon } from '@phosphor-icons/react/dist/ssr/Timer';
+import { LinearProgress } from '@mui/material';
 
 export interface TimeSpentProps {
   sx?: SxProps;
-  value: string;
+  title: string;
+  value: number;
+  label: string;
+  percentage: number;
 }
 
-export function TimeSpent({ sx, value }: TimeSpentProps): React.JSX.Element {
+export function TimeSpent({ sx, title, value, label, percentage }: TimeSpentProps): React.JSX.Element {
   return (
     <Card sx={sx}>
       <CardContent>
@@ -20,13 +24,24 @@ export function TimeSpent({ sx, value }: TimeSpentProps): React.JSX.Element {
           <Stack direction="row" sx={{ alignItems: 'flex-start', justifyContent: 'space-between' }} spacing={3}>
             <Stack spacing={1}>
               <Typography color="text.secondary" variant="overline">
-                TimeSpent
+                {title}
               </Typography>
-              <Typography variant="h4">{value}</Typography>
+              <Typography variant="h4">{value}h</Typography>
+              <Typography color="text.primary" variant="h6">
+                {percentage}%
+              </Typography>
             </Stack>
             <Avatar sx={{ backgroundColor: 'var(--mui-palette-primary-main)', height: '56px', width: '56px' }}>
               <TimerIcon fontSize="var(--icon-fontSize-lg)" />
             </Avatar>
+          </Stack>
+          <div>
+            <LinearProgress value={percentage} variant="determinate" />
+          </div>
+          <Stack sx={{ alignItems: 'left' }} direction="row" spacing={2}>
+            <Typography color="text.primary" variant="h6">
+              {label}
+            </Typography>
           </Stack>
         </Stack>
       </CardContent>
